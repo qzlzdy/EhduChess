3e67802912831799fe837e8663bb4aec1f955bae
Qzlzdy <qzlzdy@qq.com>
2022-03-27 14:55:13 +0800

    UPDATE server 22-3-27

--- server.py
+++ server.py
@@ -1,24 +1,41 @@
-import socket
+import subprocess
 
-ser = socket.socket()
-host = ''
-port = 2233
-ser.bind((host, port))
-ser.listen(5)
-print('listening 2233')
+engine = subprocess.Popen(
+    './stockfish',
+    stdin=subprocess.PIPE,
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE,
+    shell=True,
+    text=True
+)
+
+outs = engine.stdout.readline() # Stockfish {version}
+print(outs[:-1])
+
+engine.stdin.write('uci\n')
+engine.stdin.flush()
 while True:
-    cli, addr = ser.accept()
-    print('Accept ip: [{ip}], port: [{port}]'.format(ip=addr[0], port=addr[1]))
-    while True:
-        try:
-            msg = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-            count = cli.send(msg.encode())
-            print('Send {cnt} bytes'.format(cnt=count))
-            msg = cli.recv(1024)
-            if len(msg) == 0:
-                break
-            print('Received:', msg.decode())
-        except:
-            break
-    print('close connection')
-    cli.close()
+    outs = engine.stdout.readline()
+    if outs == 'uciok\n':
+        break
+print(outs[:-1])
+
+engine.stdin.write('setoption name Threads value 4\n')
+print('set Threads=4')
+engine.stdin.write('setoption name UCI_AnalyseMode value true\n')
+print('set UCI_AnalyseMode=true')
+engine.stdin.write('isready\n')
+engine.stdin.flush()
+outs = engine.stdout.readline() # readyok
+print(outs[:-1])
+
+engine.stdin.write('ucinewgame\n')
+# TODO TCP/IP
+engine.stdin.write('position startpos moves\n')
+engine.stdin.write('go depth 20\n')
+engine.stdin.flush()
+while True:
+    outs = engine.stdout.readline()
+    if outs[:8] == 'bestmove':
+        break
+print(outs[:-1])
